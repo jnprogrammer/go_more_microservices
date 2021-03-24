b0d47d2d45c3d4703e33a5736676b5b7ab0e6ce2
jnprogrammer <joshuanparker@gmail.com>
2021-03-23 23:26:37 -0400

    Instead of having two funcs for GETS and POSTS, lets have one func to do it all.

--- handlers/products.go
+++ handlers/products.go
@@ -14,10 +14,22 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(rw http.ResponseWriter, h *http.Request) {
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	//catch all
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+// Instead of having seporate funcs for GETS and POSTS, one func to do it all.
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	lp := data.GetProcuts()
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Can't marshal jason", http.StatusInternalServerError)
 	}
+
 }
